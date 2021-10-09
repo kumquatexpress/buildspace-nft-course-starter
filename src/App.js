500d0e5b1bc6485156422a3e970564aa6e74511a
Boyang Niu <boyang@squareup.com>
2021-10-09 15:38:24 -0700

    Dummy app

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xd6E6344317b744b4cD15Ef4d8f068dAc25A4377B";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [numMined, setNumMined] = useState(0);
 
   const checkIfWalletIsConnected = async () => {
     /*
@@ -37,6 +39,7 @@ const App = () => {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account)
+        setupEventListener();
     } else {
         console.log("No authorized account found")
     }
@@ -60,7 +63,8 @@ const App = () => {
       * Boom! This should print out public address once we authorize Metamask.
       */
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
+      setupEventListener();
     } catch (error) {
       console.log(error)
     }
@@ -74,29 +78,65 @@ const App = () => {
   );
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x54bd3B70513b72F7c1fa022d18E51AE98F737bFD";
-      try {
-        const { ethereum } = window;
-  
-        if (ethereum) {
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
-  
-          console.log("Going to pop wallet now to pay gas...")
-          let nftTxn = await connectedContract.makeAnEpicNFT();
-  
-          console.log("Mining...please wait.")
-          await nftTxn.wait();
-          
-          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
-  
-        } else {
-          console.log("Ethereum object doesn't exist!");
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+
+
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4";
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+          return;
         }
-      } catch (error) {
-        console.log(error)
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", async (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          let totalMined = await connectedContract.getTotalMined();
+          setNumMined(Number(totalMined));
+        });
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
       }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        let totalMined = await connectedContract.getTotalMined();
+        setNumMined(Number(totalMined));
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -109,7 +149,7 @@ const App = () => {
         <div className="header-container">
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Mined {numMined} / {TOTAL_MINT_COUNT}
           </p>
           {currentAccount === "" ? renderNotConnectedContainer() : (
             <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
